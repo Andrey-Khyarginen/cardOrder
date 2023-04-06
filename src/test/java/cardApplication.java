27783f2f0b48608989460a521993bc741bb18849
Andrey Kharginen <kut13@inbox.ru>
2023-04-06 17:18:33 +0400

    add test

---
+++ src/test/java/cardApplication.java
@@ -0,0 +1,41 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class cardApplication {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit(); // закрыли браузер
+        driver=null; // обнулили драйвер
+    }
+    @Test
+    void positiveTest() {
+
+    }
+}
